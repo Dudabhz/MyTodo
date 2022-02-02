c9a0466122c06736415942d3dcb93141ce07806f
Duda <dudabhz@gmail.com>
2022-02-02 18:15:08 -0300

    teste duda

--- Startup.cs
+++ Startup.cs
@@ -15,6 +15,7 @@ namespace MyTodo
         {
             services.AddControllers();
             services.AddDbContext<AppDbContext>();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
