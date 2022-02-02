161a07dfe730778ba9f3762524504c51a2dccba2
Duda <dudabhz@gmail.com>
2022-02-02 17:44:46 -0300

    teste

--- Startup.cs
+++ Startup.cs
@@ -21,10 +21,8 @@ namespace MyTodo
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
-            {
                 app.UseDeveloperExceptionPage();
-            }
-
+            
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
